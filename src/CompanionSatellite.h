600687ed36d7899c8f19797acc9b82046f44f74f
alex-Arc <alex@arc-light.dk>
2021-11-29 18:03:18 +0100

    version match

--- src/CompanionSatellite.h
+++ src/CompanionSatellite.h
@@ -3,6 +3,7 @@
 
 #include <Arduino.h>
 #include <string>
+#include <vector>
 
 #define Use_NativeEthernet
 #ifdef Use_NativeEthernet
@@ -18,8 +19,52 @@ const int VERSION_PATCH = 0;
 class CompanionSatellite
 {
 private:
+    const std::vector<std::string> CMDS = {
+        "QUIT",
+        "PING",
+        "PONG",
+        "ADD-DEVICE",
+        "REMOVE-DEVICE",
+        "KEY-PRESS",
+        "KEY-STATE",
+        "KEYS-CLEAR",
+        "BRIGHTNESS"};
+
+    enum CompanionSatelliteCommand
+    {
+        NONE = -1,
+        QUIT,
+        PING,
+        PONG,
+        ADD_DEVICE,
+        REMOVE_DEVICE,
+        KEY_PRESS,
+        KEY_STATE,
+        KEYS_CLEAR,
+        BRIGHTNESS,
+    };
+
+    enum CompanionSatelliteStatus
+    {
+        NoConnection = -2,
+        VersionMiss = -1,
+        Starting = 1,
+        ConnectionActive,
+        AwaitingBegin,
+        AddingDevice,
+        Connected,
+    };
+
+    typedef struct
+    {
+        CompanionSatelliteCommand cmd;
+        std::vector<std::string> args;
+    } CompanionSatellitePacket_t;
+
     bool initSocket();
+    bool sendCommand(CompanionSatellitePacket_t);
     int readPacket();
+    CompanionSatellitePacket_t parseParameters();
     void sendPing();
     void addDevice();
 
@@ -30,17 +75,9 @@ private:
     elapsedMillis _timeout;
 
     uint8_t buf[BUFFER_SIZE];
-    std::string _str;
 
-    enum CompanionSatelliteStatus
-    {
-        NoConnection = -2,
-        VersionMiss = -1,
-        Starting = 1,
-        ConnectionActive,
-        AwaitingBegin,
-        Connected,
-    };
+    //TODO: maybe pass it as a parmater
+    std::string _str;
 
     CompanionSatelliteStatus _status;
 
@@ -80,6 +117,69 @@ int CompanionSatellite::readPacket()
     return len;
 }
 
+CompanionSatellite::CompanionSatellitePacket_t CompanionSatellite::parseParameters()
+{
+    CompanionSatellitePacket_t pack;
+    pack.cmd = CompanionSatelliteCommand::NONE;
+
+    int cmd_size = _str.find_first_not_of(" ");
+    if (cmd_size){
+        Serial.println(_str.substr(0, cmd_size).c_str());
+        // switch (_str.substr(0, cmd_size))
+        // {
+        // case /* constant-expression */:
+        //     /* code */
+        //     break;
+        
+        // default:
+        //     break;
+        // }
+    }
+
+    return pack;
+}
+
+bool CompanionSatellite::sendCommand(CompanionSatellitePacket_t packet)
+{
+    Serial.println("sendCommand()");
+
+    if (_status >= CompanionSatelliteStatus::AwaitingBegin)
+    {
+        Serial.print("SENDING: ");
+
+        _socket->print(CMDS[packet.cmd].c_str());
+        Serial.print(CMDS[packet.cmd].c_str());
+        _socket->print(" ");
+        Serial.print(" ");
+        for (std::vector<std::string>::iterator it = packet.args.begin(); it != packet.args.end(); ++it)
+        {
+            _socket->print(it->c_str());
+            Serial.print(it->c_str());
+
+            _socket->print(" ");
+            Serial.print(" ");
+        }
+        _socket->print("\n");
+        Serial.print("\n");
+    }
+    return 1;
+}
+
+void CompanionSatellite::addDevice()
+{
+    Serial.println("addDevice()");
+    CompanionSatellitePacket_t pack;
+    pack.cmd = CompanionSatelliteCommand::ADD_DEVICE;
+    pack.args.push_back("DEVICEID=00000");
+    pack.args.push_back("PRODUCT_NAME=\"Satellite Arduino\"");
+    pack.args.push_back("KEYS_TOTAL=2");
+    pack.args.push_back("KEYS_PER_ROW=2");
+    pack.args.push_back("BITMAPS=false");
+    pack.args.push_back("COLORS=true");
+    pack.args.push_back("TEXT=false");
+    sendCommand(pack);
+}
+
 int CompanionSatellite::maintain()
 {
     switch (_status)
@@ -115,18 +215,28 @@ int CompanionSatellite::maintain()
                 std::string minor = _str.substr(dot1, 1);
                 std::string patch = _str.substr(dot2, 1);
 
-                Serial.printf("major=%s minor=%s patch=%s", major.c_str(), minor.c_str(), patch.c_str());
+                Serial.printf("major=%s minor=%s patch=%s \n", major.c_str(), minor.c_str(), patch.c_str());
 
                 if (atoi(major.c_str()) == VERSION_MAJOR && atoi(minor.c_str()) >= VERSION_MINOR)
                 {
-                    _status = CompanionSatelliteStatus::Connected;
-
+                    Serial.println("version match");
+                    _status = CompanionSatelliteStatus::AddingDevice;
+                    addDevice();
                 }
             }
         }
         if (_timeout > RECONNECT_DELAY)
             _status = CompanionSatelliteStatus::Starting;
         break;
+    }
+    case CompanionSatelliteStatus::AddingDevice:
+    {
+        if (readPacket())
+        {
+            parseParameters();
+        }
+        // if (_timeout > RECONNECT_DELAY)
+        //     _status = CompanionSatelliteStatus::Starting;
     }
 
     default:
