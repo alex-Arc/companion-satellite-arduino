3b043e3584f6b52d88f6890ba46461c973e95d4d
Alex <ac@omnivox.dk>
2022-06-19 16:38:19 +0200

    handleCommand cmd body split

--- src/CompanionSatellite.h
+++ src/CompanionSatellite.h
@@ -14,6 +14,7 @@ private:
     unsigned long _lastReceivedAt;
     std::string receiveBuffer;
 
+    void handleCommand(std::string line);
 public:
     void _handleReceivedData(char *data);
     bool connected();
@@ -32,10 +33,56 @@ void CompanionSatellite::_handleReceivedData(char *data)
         std::string line = this->receiveBuffer.substr(offset, i - offset);
         offset = i + 1;
         Serial.printf("LINE >%s<\n", line.data());
-        // this->handleCommand(line.toString().replace(/\r /, ''));
+        this->handleCommand(line); //TODO: remove potential \r
     }
     this->receiveBuffer.erase(0, offset);
 }
+
+void CompanionSatellite::handleCommand(std::string line) {
+		size_t i = line.find_first_of(' ');
+		std::string cmd = (i == std::string::npos) ? line : line.substr(0, i-1);
+		std::string body = (i == std::string::npos) ? "" : line.substr(i + 1);
+
+        Serial.printf("CMD: >%s<\tBODY: >%s<\n", cmd.data(), body.data());
+		
+        /*
+        const params = parseLineParameters(body)
+
+		switch (cmd.toUpperCase()) {
+			case 'PING':
+				this.socket?.write(`PONG ${body}\n`)
+				break
+			case 'PONG':
+				// console.log('Got pong')
+				this._pingUnackedCount = 0
+				break
+			case 'KEY-STATE':
+				this.handleState(params)
+				break
+			case 'KEYS-CLEAR':
+				this.handleClear(params)
+				break
+			case 'BRIGHTNESS':
+				this.handleBrightness(params)
+				break
+			case 'ADD-DEVICE':
+				this.handleAddedDevice(params)
+				break
+			case 'REMOVE-DEVICE':
+				console.log('Removed device: ${body}')
+				break
+			case 'BEGIN':
+				console.log(`Connected to Companion: ${body}`)
+				break
+			case 'KEY-PRESS':
+				// Ignore
+				break
+			default:
+				console.log(`Received unhandled command: ${cmd} ${body}`)
+				break
+		}
+        */
+	}
 
 bool CompanionSatellite::connected()
 {
